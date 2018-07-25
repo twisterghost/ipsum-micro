c12645474afe4eb65c9cda1e9b8835b700a97213
Michael Barrett <twisterghost@gmail.com>
2018-07-25 16:23:45 -0400

    Clamp to 1-1000 sentences

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const url = require('url');
 const loremIpsum = require('lorem-ipsum');
 
+function clamp(val, min, max) {
+  return Math.max(Math.min(val, max), min);
+}
+
 module.exports = async (request) => {
-  const count = parseInt(url.parse(request.url).query) || 10;
+  const count = clamp(parseInt(url.parse(request.url).query) || 10, 1, 1000);
   console.log(`Generating ${count} lines of lorem ipsum...`);
   return loremIpsum({
     count,
