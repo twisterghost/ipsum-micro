5881d9097b6c56002c96878bcaa5873ac833d2cb
Michael Barrett <twisterghost@gmail.com>
2019-05-20 20:56:00 -0400

    Configure for now v2

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function clamp(val, min, max) {
   return Math.max(Math.min(val, max), min);
 }
 
-module.exports = async (request) => {
+module.exports = (request, response) => {
   const count = clamp(parseInt(url.parse(request.url).query) || 10, 1, 1000);
   console.log(`Generating ${count} lines of lorem ipsum...`);
-  return loremIpsum({
+  response.end(loremIpsum({
     count,
     unit: 'sentences',
-  });
+  }));
 };
